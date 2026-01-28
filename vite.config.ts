d38325060a324a4f7c46bac213e418ecbff019ab
Yasir1986 <yasir.akbar86@hotmail.com>
2026-01-28 16:08:57 +0000

    Ready for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',  // Netlify
+  build: {
+    outDir: 'dist',
+    sourcemap: false  
+  },
   plugins: [react()],
 })
